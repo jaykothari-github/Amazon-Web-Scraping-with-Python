def28977ae9ae73face7f9a8ab5c4a0320a16083
Jay Kothari <jaykothari49@gmail.com>
2021-12-15 02:09:38 +0530

    Unlock Pipelines

--- Amazon_Scrap/pipelines.py
+++ Amazon_Scrap/pipelines.py
@@ -10,4 +10,5 @@ from itemadapter import ItemAdapter
 
 class AmazonScrapPipeline:
     def process_item(self, item, spider):
+        print('----------------------------------')
         return item
--- Amazon_Scrap/settings.py
+++ Amazon_Scrap/settings.py
@@ -63,9 +63,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'Amazon_Scrap.pipelines.AmazonScrapPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'Amazon_Scrap.pipelines.AmazonScrapPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
