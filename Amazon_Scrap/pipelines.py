174e8a8b408631edce079adac12520c04b269b01
Jay Kothari <jaykothari49@gmail.com>
2021-12-19 16:53:37 +0530

    Setting up the database with SQLite

--- Amazon_Scrap/pipelines.py
+++ Amazon_Scrap/pipelines.py
@@ -6,9 +6,34 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlite3
 
 
 class AmazonScrapPipeline:
+
+    def __init__(self):
+        self.connect()
+        self.create_table()
+
+    def connect(self):
+        self.conn = sqlite3.connect('Database.sqlite')
+        self.cur = self.conn.cursor()
+
+    def create_table(self):
+        self.cur.execute('DROP TABLE IF EXISTS Amazon')
+        self.cur.execute("""CREATE TABLE Amazon (
+    Title VARCHAR(128),
+    price VARCHAR(128),
+    img VARCHAR
+)""")
+
+
     def process_item(self, item, spider):
-        print('----------------------------------')
+        
+        self.insert_data(item)
+        self.conn.commit()
+
         return item
+
+    def insert_data(self,item):
+        self.cur.execute('INSERT INTO Amazon VALUES (?,?,?)',(item['title'],item['price'],item['img']))
