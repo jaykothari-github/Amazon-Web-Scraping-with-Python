9bad7e6860c40f3b5057bfbf3f0c26c07540b7ee
Jay Kothari <jaykothari49@gmail.com>
2021-12-19 16:54:36 +0530

    Enable user agent

--- Amazon_Scrap/settings.py
+++ Amazon_Scrap/settings.py
@@ -15,7 +15,7 @@ NEWSPIDER_MODULE = 'Amazon_Scrap.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'Amazon_Scrap (+http://www.yourdomain.com)'
-# USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
