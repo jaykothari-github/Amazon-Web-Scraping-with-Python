8f5dc71f5551b71a5655a2df2c290e4448cd542f
Jay Kothari <jaykothari49@gmail.com>
2021-12-19 16:52:15 +0530

    Fixing Items Error

--- Amazon_Scrap/spiders/Amazon.py
+++ Amazon_Scrap/spiders/Amazon.py
@@ -6,8 +6,10 @@ class AmazonSpider(scrapy.Spider):
     name = 'Amazon'
     # allowed_domains = ['Amazon.in']
     start_urls = ['https://www.amazon.in/s?k=mobile+phone&i=electronics&crichd=8JAMGTSNJLC5&sprefix=mobil%2Celectronics%2C527&ref=nb_sb_ss_ts-doa-p_3_5']
+    k = 2
 
     def parse(self, response):
+        items = AmazonScrapItem()
         
         val = response.css('.s-border-top')
         
@@ -21,9 +23,17 @@ class AmazonSpider(scrapy.Spider):
             except: 
                 img = 'No Image'
                 
-            items = AmazonScrapItem()
             items['title'] = title
             items['price'] = price
             items['img'] = img
 
             yield items
+
+        # check = response.css('.a-pagination a::attr(href)').get()
+
+        if self.k < 7:
+            
+            next_url = f'https://www.amazon.in/s?k=mobile+phone&i=electronics&page={self.k}&crichd=8JAMGTSNJLC5&qid=1639909083&sprefix=mobil%2Celectronics%2C527&ref=sr_pg_{self.k-1}'
+            print(f'----------Upto Next page > {self.k} ---------------------------------')
+            self.k += 1
+            yield response.follow(next_url,callback = self.parse)
